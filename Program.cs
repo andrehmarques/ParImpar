34233e7937b89fb71104918e3786d06f162f2bea
Andreh Marques <andrehmarquez@gmail.com>
2025-05-12 08:55:40 -0300

    Programa Finalizado

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿int numero;
 
-Console.Write("Digite um número: ");
-numero = int.Parse(Console.ReadLine()!);
+Console.Write("Digite um número para saber se ele é Par ou Impar: ");
+
+while(!int.TryParse(Console.ReadLine(), out numero) || numero < 0)
+{
+    Console.WriteLine("O número deve ser maior que 0, por favor digite outro número: ");
+}
 
 if(numero %2 == 0)
 {
-    Console.WriteLine($"{numero} é par");
+    Console.WriteLine($"{numero} é um número par");
 }
 
 else
 {
-    Console.WriteLine($"{numero} é impar");
+    Console.WriteLine($"{numero} é um número impar");
 }
