13de18c3120adf9ed5383461ecfa2714ad81f511
Andreh Marques <andrehmarquez@gmail.com>
2025-05-12 08:20:46 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = int.Parse(Console.ReadLine()!);
+
+if(numero %2 == 0)
+{
+    Console.WriteLine($"{numero} é par");
+}
+
+else
+{
+    Console.WriteLine($"{numero} é impar");
+}
